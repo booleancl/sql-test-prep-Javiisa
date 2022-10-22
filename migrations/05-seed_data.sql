392a48fd8f9bd4e52fa55e111bf7a2a2b6702b05
charlotte <charlottediaz88@gmail.com>
2022-10-22 11:44:42 -0300

    querys sql

--- migrations/05-seed_data.sql
+++ migrations/05-seed_data.sql
@@ -2,5 +2,5 @@
 
 \COPY customers FROM 'C:/Users/user/intro_dev/sql-test-prep-Javiisa/data/customers.csv'csv HEADER;
 \COPY products FROM 'C:/Users/user/intro_dev/sql-test-prep-Javiisa/data/products.csv'csv HEADER;
-\COPY details FROM 'C:/Users/user/intro_dev/sql-test-prep-Javiisa/data/purchase_details.csv'csv HEADER;
 \COPY purchases FROM 'C:/Users/user/intro_dev/sql-test-prep-Javiisa/data/purchases.csv'csv HEADER;
+\COPY details FROM 'C:/Users/user/intro_dev/sql-test-prep-Javiisa/data/purchase_details.csv'csv HEADER;
